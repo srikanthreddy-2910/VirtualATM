5300a28ad3655c776a0af194afec7727beee7614
Srikanth <srikanthreddylokurthi@gmail.com>
2026-01-14 22:30:16 +0530

    testing fronted on server

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://virtual-atm.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
