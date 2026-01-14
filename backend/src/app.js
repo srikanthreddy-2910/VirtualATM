2cb502ec0a32b15fb482e29f52d33abd74ee48a2
Srikanth <srikanthreddylokurthi@gmail.com>
2026-01-14 16:12:47 +0530

    connection error

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,14 +5,12 @@ const app = express();
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://virtual-atm.vercel.app"
-    ],
+    origin: ["http://localhost:5174", "https://virtual-atm.vercel.app"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
-
 
 app.use(express.json());
 
@@ -22,6 +20,6 @@ app.get("/", (req, res) => {
 app.use("/api/atm", require("./routes/atm.routes"));
 app.use("/api/auth", require("./routes/auth.routes"));
 app.use("/api/accounts", require("./routes/account.routes"));
-app.use("/api/transactions", require("./routes/transaction.routes")); 
+app.use("/api/transactions", require("./routes/transaction.routes"));
 
 module.exports = app;
